805c2a18fc62704155da1f44f77e91ac16bea5db
Daniel <daniel.opazo.baer@gmail.com>
2015-09-17 23:57:11 -0400

    smtp program for 3010 lab

---
+++ emailTest.py
@@ -0,0 +1,17 @@
+import smtplib
+from email.mime.text import MIMEText
+
+message = "Test Email please ignore"
+
+msg = MIMEText(message)
+msg['Subject'] = 'Raspberry Pi Python test'
+msg['From'] = 'daniel\'s raspberry pi'
+#msg['To'] = 'samson.truong@gmail.com'
+msg['To'] = 'daniel.opazo.baer@gmail.com'
+
+#s = smtplib.SMTP('smtp.gmail.com', 465)
+s = smtplib.SMTP('localhost')
+#s.login('daniel.opazo.baer@gmail.com', 'dmob1111')
+s.login('pi', 'raspberry')
+s.sendmail(msg['From'], msg['To'], msg.as_string())
+s.quit()
